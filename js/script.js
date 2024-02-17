6ebc519585569fce70da06354b25864a860e83fc
Vadym Sabalaiev <103932394+VSABALAIEV78@users.noreply.github.com>
2024-02-17 23:29:59 +0200

    OL project 2024

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.navi'),
-        menuItem = document.querySelectorAll('.navi_item'),
-        hamburger = document.querySelector('.hamburger');
+$(document).ready(function () {
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('navi_active');
+    //change active class for .catalog_item
+    $('div.category_wrapper').on('click', 'div:not(div.category_item_active)', function () {
+        $(this)
+            .addClass('category_item_active').siblings().removeClass('category_item_active')
+            .closest('section.practice').find('div.practice_wrapper').removeClass('practice_wrapper_active').eq($(this).index()).addClass('practice_wrapper_active');
     });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('navi_active');
-        })
-    })
-})
+    
+});
